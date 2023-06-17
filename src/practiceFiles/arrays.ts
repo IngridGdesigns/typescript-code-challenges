40867b67984c2039c32d629e4b7d737fb8fe459f
IngridGdesigns <garciai@protonmail.com>
2023-06-16 22:23:04 -0700

    notes and code

--- src/practiceFiles/arrays.ts
+++ src/practiceFiles/arrays.ts
@@ -1,6 +1,6 @@
-// Working with arrays
+// Working with arrays exercise
 
-const friends = [
+const friends: { name: string; position: string }[] = [
   {
     name: 'Wesley',
     position: 'musician',
@@ -11,10 +11,32 @@ const friends = [
   },
 ];
 
-// Some tslint rules are disabling use of [], example message: 
+// Some tslint rules are disabling use of [], example message:
 // Array type using 'T[]' is forbidden for non - simple types.Use 'Array<T>'
-function getFriends(friends: Array<{ name: string; position: string }>): any {
+// instead write: function getFriends(friends: Array<{ name: string; position: string }>): any {
+function getFriends(friends: { name: string; position: string }[]): any {
   return friends.filter((friend) => friend.position === 'musician');
 }
 
 console.log(getFriends(friends));
+
+type User = {
+  id: number;
+  name: string;
+};
+
+type UserGroup = {
+  [key: string]: User;
+};
+
+var obj: UserGroup = {
+  '0': { id: 0, name: 'Tivoli' },
+  '1': { id: 1, name: 'Stuart' },
+  '2': { id: 2, name: 'Carlo' },
+};
+// or if you make it an array
+var arr: User[] = [
+  { id: 0, name: 'Available' },
+  { id: 1, name: 'Ready' },
+  { id: 2, name: 'Started' },
+];
