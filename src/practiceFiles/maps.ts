b4a0b60d42e8b7c1f3549046b9005118ac1f040b
IngridGdesigns <garciai@protonmail.com>
2023-06-22 11:56:05 -0700

    added array, instantiated new Map

---
+++ src/practiceFiles/maps.ts
@@ -0,0 +1,26 @@
+const cats = [
+  {
+    name: 'max',
+    coastSize: 'long',
+    breed: 'himalayan',
+    color: 'black',
+  },
+  {
+    name: 'don',
+    coastSize: 'long',
+    breed: 'balinese',
+    color: 'colorpoint',
+  },
+  {
+    name: 'shadow',
+    coastSize: 'short',
+    breed: 'brazilian shorthair',
+    color: 'chocolate',
+  },
+];
+
+
+
+// you must always explicitly create a new instance of a Map.
+
+let catMap = new Map();
