96ae3f8c855c13bb7fe69eee0d7b9ad478ee94a8
IngridGdesigns <garciai@protonmail.com>
2023-06-22 12:59:14 -0700

    practicing sort, spread operator and chaining methods

--- src/practiceFiles/maps.ts
+++ src/practiceFiles/maps.ts
@@ -54,7 +54,6 @@ console.log(filters);
 filters.clear(); // delete all key-value pairs with clear() method
 console.log(filters.get('color'));
 
-
 // Map can take in different type of keys, such as integers
 let errors = new Map([
   [100, 'Invalid name'],
@@ -62,5 +61,82 @@ let errors = new Map([
   [200, 'Invalid color'],
 ]);
 console.log(errors.get(100));
-// can also get the keys from a Map 
+// can also get the keys from a Map
 console.log(errors.keys()); // The MapIterator is what will allow us to loop through data.
+
+const map = new Map();
+
+const foods = { dinner: 'Quesadillas', lunch: 'Soup', breakfast: 'Oatmeal' };
+const yummyFood = {};
+
+map.set(yummyFood, foods);
+
+for (const [key] of map) {
+  console.log(map.get(key));
+}
+
+const f = {
+  color: 'black',
+  breed: 'labrador',
+};
+
+function getAppliedFilters(f: any): any {
+  const keys = Object.keys(f); // lengthy without a map
+  keys.sort();
+  console.log(keys);
+  const applied: string[] = [];
+  for (const key of keys) {
+    applied.push(`${key}:${f[key]}`);
+  }
+  return `Your filters are: ${applied}.`;
+}
+
+// The for ... of syntax is very simple. It returns each value in the collection one at a time
+const fruitFilters = new Map()
+  .set('color', 'red')
+  .set('type', 'apple')
+  .set('seedless', false);
+
+function checkFilters(fruitFilters: Map<string, string>): void {
+  for (const entry of fruitFilters) {
+    console.log(entry); // It’s a pair of the key-value, translating to an array
+  }
+}
+
+checkFilters(fruitFilters);
+console.log(fruitFilters.entries());
+
+const veggies = new Map().set('color', 'green').set('type', 'squash');
+
+// uses destructuring, helps you avoid breaking apart your data structure.
+function getVeggies(veg: Map<string, string>): string {
+  const applied = [];
+  for (const [key, value] of veg) {
+    applied.push(`${key}:${value}`);
+  }
+  return `Your filters are: ${applied.join(', ')}.`;
+}
+
+console.log(getVeggies(veggies), '\n');
+
+const veggies2 = new Map().set('color', 'purple').set('type', 'eggplant');
+// sorting by key with the spread operator
+function sortByKey(a: [string, string], b: [string, string]) {
+  return a[0] > b[0] ? 1 : -1;
+}
+function getSortedAppliedFilters(veggies2: Map<string, string>): string {
+  //   const applied = [];
+  //   for (const [key, value] of [...veggies].sort(sortByKey)) {
+  //     applied.push(`${key}:${value}`);
+  //   }
+  // using map method to loop through
+  const applied = [...veggies2]
+    .sort(sortByKey)
+    .map(([key, value]) => {
+      return `${key}:${value}`;
+    })
+    .join(', '); //and because everything is an arry you can chain sort and join methods
+  return `Your filters are: ${applied}\n`;
+}
+
+console.log(getSortedAppliedFilters(veggies2));
