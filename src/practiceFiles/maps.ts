8cb379593cc35729bfeb425a3533a5a32d0a6d36
IngridGdesigns <garciai@protonmail.com>
2023-06-22 12:12:40 -0700

    display different ways of adding key-value pairs, deleting

--- src/practiceFiles/maps.ts
+++ src/practiceFiles/maps.ts
@@ -19,8 +19,37 @@ const cats = [
   },
 ];
 
-
-
 // you must always explicitly create a new instance of a Map.
-
 let catMap = new Map();
+
+catMap.set('breed', 'balinese');
+
+console.log(catMap);
+
+console.log(catMap.get('breed'));
+
+//ading data by chaining directly from the creation of the new instance
+let chainingMapCats = new Map()
+  .set('breed', 'siamese')
+  .set('coatSize', 'long')
+  .set('color', 'colorpoint');
+
+console.log(chainingMapCats);
+console.log('Getting the coat size:');
+console.log(chainingMapCats.get('coatSize'));
+
+/* you can pass an array of pairs with the first element being
+ a key and the second element being a value.*/
+let filters = new Map([
+  ['breed', 'bengal'],
+  ['coatSize', 'short'],
+  ['color', 'spotted/marbled'],
+]);
+
+console.log(filters);
+console.log(filters.get('color'));
+filters.delete('color');
+console.log(filters.get('color'));
+console.log(filters);
+filters.clear(); // delete all key-value pairs with clear() method
+console.log(filters.get('color'));
