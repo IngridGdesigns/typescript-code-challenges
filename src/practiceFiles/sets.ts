f52b3c2b623609593452139245b41a290864b79d
IngridGdesigns <garciai@protonmail.com>
2023-06-23 02:16:56 -0700

    added notes and practice code

--- src/practiceFiles/sets.ts
+++ src/practiceFiles/sets.ts
@@ -1,4 +1,33 @@
 // Practicing Sets
+// The main difference is that instead of taking an array of pairs, you can
+// create a new instance of Set by passing a flat array as an argument.
+
+const favCookies = ['butter', 'lemon', 'butter'];
+const unique = new Set(favCookies);
+console.log(unique);
+
+// using spread operator on Set
+const allFavCookies = ['lemon', 'butter', 'lemon', 'chocolate', 'shortbread'];
+
+function getUniqueCookies(attributes: string[]): string[] {
+  const favCookies = [...new Set(attributes)];
+  return favCookies;
+}
+
+console.log(getUniqueCookies(allFavCookies));
+
+// A set can keep only one of each value. If you try to add a value that isn’t yet in the set,
+// it will be added. If you try to add a value that already exists, it will be ignored.
+// Order is preserved, and the initial point a value is added will remain.If you try to add an
+// item that’s there already, it keeps the original position.
+
+let travelBag = new Set();
+travelBag.add('towel');
+travelBag.add('socks');
+travelBag.add('face wipes');
+travelBag.add('towel');
+console.log(travelBag);
+
 
 const booksToRead = [
   {
@@ -43,13 +72,15 @@ const booksToRead = [
 //   return books.map((book: { type: any; }) => book.type);
 // }
 
-function getAllAuthors(books: any): any {
+function getAllAuthors(books: any) {
   const unique = new Set();
   for (const book of books) {
     unique.add(book.author);
   }
   return [...unique];
 }
+
+console.log(getAllAuthors(booksToRead));
 
 function getUniqueBooks(books: any[]): string[] {
   let unique: string[] = [];
@@ -64,8 +95,6 @@ function getUniqueBooks(books: any[]): string[] {
 
 console.log(booksToRead.forEach((book) => book.author));
 
-const types = getAllAuthors(booksToRead);
-console.log(getAllAuthors(booksToRead));
 
 let ans = new Set([1, 1, 2, 2, 3, 5, 8]);
 const mulitply = [...ans].map((x) => x * 2);
