3d14bd9cd8b676f29537d794cc4096e731e461ca
IngridGdesigns <garciai@protonmail.com>
2023-06-23 03:28:06 -0700

    issues of chaining

--- src/practiceFiles/conditionals.ts
+++ src/practiceFiles/conditionals.ts
@@ -20,7 +20,7 @@ const color = balance > 0 ? 'green' : 'red';
 // }
 console.log(color);
 
-// Equivalency - a value that’s equivalent if it’s the same, but of a different 
+// Equivalency - a value that’s equivalent if it’s the same, but of a different
 // type & is checked with ‘==‘.
 // console.log(4 == '4'); // errors in TypeScript but not JavaScript
 // output: true
@@ -43,7 +43,7 @@ if ('') {
 // Values that are falsey:  0, null, and an empty string.
 
 // Arrays and objects, even empty arrays and objects, are always truthy.
-// So they have to be checked with [].length or Object.keys({}).length to 
+// So they have to be checked with [].length or Object.keys({}).length to
 // get a truthy number
 const person = {
   name: 'Eric',
@@ -54,24 +54,23 @@ if (!person.equipmentTraining) {
   console.log(`${person.name} is not authorized to operate machinery`);
 }
 
-// The most common problem occurs when you’re testing existence in an array 
+// The most common problem occurs when you’re testing existence in an array
 // by checking the index of a value:
-console.log(['c', 'd'].indexOf('c')) // 0 which is falsy
+console.log(['c', 'd'].indexOf('c')); // 0 which is falsy
 
 /* use a strict equivalency check to make sure the value is there and it’s in
 the format you want.If you use strict equivalency, you can guard against a 
 situation where someone sets employee.equipmentTraining to 'Not Trained'*/
 const baker = {
-    name: 'Panfilo',
-    bruleeTraining: true,
+  name: 'Panfilo',
+  bruleeTraining: true,
 };
 
-
 function checkAuthorization() {
-    if (baker.bruleeTraining !== true) {
-        return 'Not authorized to operate blow torch';
-    }
-    return `Hello, ${baker.name}`
+  if (baker.bruleeTraining !== true) {
+    return 'Not authorized to operate blow torch';
+  }
+  return `Hello, ${baker.name}`;
 }
 
 // baker.bruleeTraining = 'Not Trained';
@@ -84,3 +83,47 @@ console.log(checkAuthorization());
 //   console.log('Bye');
 // }
 // output: Hello
+
+// Ternary practice
+// ternary expressions allow your code to be not just more simple, but also more predictable,
+// because they cut down on the number of variables that are being reassigned.
+// before ternary
+const active = true;
+let display;
+
+if (active) {
+  display = 'blue';
+} else {
+  display = 'black';
+}
+console.log(display);
+
+// after
+const isThisActive = active ? 'blue' : 'black';
+console.log(display);
+
+//rewrite
+const title = 'cook';
+let permissions;
+if (title === 'cook') {
+  permissions = ['time', 'pay'];
+} else {
+  permissions = ['time'];
+}
+console.log(permissions);
+
+// with ternary
+const typeOfPermissions = title === 'cook' ? ['time', 'pay'] : ['time'];
+
+// avoid chaining multiple ternary expressions together, it makes it undreadable
+function getJobTitle(title: string): string[] {
+  const permissions =
+    title === 'supervisor' || title === 'teacher'
+      ? title === 'teacher'
+        ? ['time', 'overtimeAuthorization', 'pay']
+        : ['time', 'overtimeAuthorization']
+      : ['time'];
+  return permissions; // undreadable and ugly looking
+}
+console.log(getJobTitle('manager'));
+console.log(getJobTitle('supervisor'));
