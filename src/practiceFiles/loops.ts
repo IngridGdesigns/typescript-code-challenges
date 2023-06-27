e67d880e4cfcc951eff50a368a065d1db9e2894d
IngridGdesigns <garciai@protonmail.com>
2023-06-27 11:33:49 -0700

    add notes

--- src/practiceFiles/loops.ts
+++ src/practiceFiles/loops.ts
@@ -1,3 +1,4 @@
 // For loops are necessary but use the right tool for the problem
-// you’re going for simplicity, readability, and predictability, and traditional loops don’t 
-// meet those goals.Array methods do. 
+// you’re going for simplicity, readability, and predictability, and traditional loops don’t
+// meet those goals.Array methods do.
+
