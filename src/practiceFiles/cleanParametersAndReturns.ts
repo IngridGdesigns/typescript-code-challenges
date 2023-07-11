7192c0fba8df4c12975ddcefd5351249dee37870
IngridGdesigns <garciai@protonmail.com>
2023-07-11 12:02:45 -0700

    destructuring and renaming keys

--- src/practiceFiles/cleanParametersAndReturns.ts
+++ src/practiceFiles/cleanParametersAndReturns.ts
@@ -15,3 +15,48 @@ function emailGreeting(
   return `Hello ${name} from ${location}, welcome!`;
 }
 console.log(emailGreeting(undefined, 'mars'));
+
+// Default parameters aren’t helpful if you want to skip a parameter. you could use undefine
+
+// Destructuring allows you to create a variable with the same name as an object’s key assigned
+// with the value from the object.
+const flowerGarden = {
+  photographer: 'Isa',
+};
+const { photographer } = flowerGarden;
+console.log(photographer);
+
+/*
+- First, you still have to declare a variable type. Preferably a const.
+- Second, the assignment variable must match the key in the object.
+- It’s set against the object. You are merely assigning a variable. 
+  The curly braces merely signal the value that variable should use is inside an object.
+*/
+
+// rest operator
+// You can collect any additional values into a new variable using three dots (...)
+// followed by the variable name
+
+const gardens = {
+  gardener: 'Lily',
+  equipment: 'Rake',
+  shift: 'Mornings',
+};
+
+const { gardener, ...additional } = gardens;
+console.log(additional);
+
+// Assign a key to a different variable name
+// This is useful in situations where the key name is taken by a previously defined
+// variable or you just don’t like the key name and you want something more expressive.
+
+const cookieMakingWorkshop = {
+  baker: 'Nitza',
+  equipment: 'cookie sheets',
+  cookieType: 'chocolate chip',
+  src: '/cookie-Nitza-Baking.jpg',
+};
+
+const { src: photo } = cookieMakingWorkshop
+console.log(photo);
+// console.log(src); src will be undefined
