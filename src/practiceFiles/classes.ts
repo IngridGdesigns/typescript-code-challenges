a8e99a7efaf99eb80e77dd841ddda533df7eb214
IngridGdesigns <garciai@protonmail.com>
2023-07-31 16:11:28 -0700

    added class methods and name change

--- src/practiceFiles/classes.ts
+++ src/practiceFiles/classes.ts
@@ -1,4 +1,3 @@
-
 /*
 Building readable Classes..
 (arrow functions behave differently in classes than normal functions)
@@ -8,24 +7,54 @@ the constructor, and then field needs to be initialized in the constructor itsel
 Note that inside a method body, it is still mandatory to access fields and other methods
 via "this". 
 */
-class Garden {
+class GardenStore {
   amount: number;
   expiration: string;
   constructor(amount: number, expiration?: string) {
     this.amount = amount;
     this.expiration = expiration || 'one week';
   }
-    
-    getAmountText() {
-        return `${this.amount}`;
-    }
 
-    getExpirationMessage() {
-        return `This produce expires in ${this.expiration}`;
-    }
+  getAmountText() {
+    return `${this.amount}`;
+  }
+
+  getExpirationMessage() {
+    return `This produce expires in ${this.expiration}`;
+  }
 }
 
-const garden = new Garden(4);
+const gardenStore = new GardenStore(4);
 
-console.log(garden.getAmountText());
-console.log(garden.getExpirationMessage());
+console.log(gardenStore.getAmountText());
+console.log(gardenStore.getExpirationMessage());
+
+/* Use Inheritance with caution...
+ */
+class Discount extends GardenStore {
+  amount!: number;
+  expiration: string;
+  constructor(amount: number, expiration?: string) {
+    super(amount);
+    this.expiration = expiration || '3 days';
+  }
+  getExpirationMessage(): string {
+    return `This is a offer, it expires in ${this.expiration}`;
+  }
+
+  isRewardsEligible() {
+    return this.amount > 20;
+  }
+}
+
+const discount = new Discount(8);
+console.log(discount.amount);
+console.log(discount.getExpirationMessage());
+
+/*
+JavaScript is a prototype language. When you create a new instance, you aren’t copying 
+methods. You’re creating a link to a prototype. When you call a method on an instance of 
+an object, you’re calling it from the prototype, which is itself an object instance 
+(not a blueprint).
+https://medium.com/javascript-scene/master-the-javascript-interview-what-s-the-difference-between-class-prototypal-inheritance-e4cd0a7562e9
+*/
