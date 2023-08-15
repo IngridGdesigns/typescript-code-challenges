f0c15978cfbecca7b580cb9dc5a0d035c11d9101
IngridGdesigns <garciai@protonmail.com>
2023-08-15 16:09:39 -0700

    added function and link to Math.floor

--- src/04-binarySearchOfAnArray.ts
+++ src/04-binarySearchOfAnArray.ts
@@ -3,16 +3,40 @@ To-do
 pseudocode of binary search:
 
 1 - Let min = 1 and max = n.
-2 - Guess the average of max and min, rounded down so that it is an integer.
-3 - If array[guess] equals target, stop. You found it! Return guess
-4 - If the guess was too low, array[guess] < target, 
+2 - If max<min, then stop; target is not present in array. Return -1.
+3 - Guess the average of max and min, rounded down so that it is an integer.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+4 - If arr[guess] equals target, stop. You found it! Return guess
+5 - If the guess was too low, array[guess] < target, 
     set min to be one larger than the guess, (set min = guess + 1).
-5 - If the guess was too high, set max to be one smaller than the guess.
+6 - If the guess was too high, set max to be one smaller than the guess.
     (set max = guess - 1)
-6 - Go back to step two.
+7 - Go back to step two.
 */
 
-function binarySearch(targetValue: number, array: string[]): number {
-    let index = 1;
-    return index; // return index of location where the target value was found
+function binarySearch(arr: number[], targetValue: number): string {
+  let min = 0;
+  let max = arr.length - 1;
+  //  What would happen if the number you are looking for is not in the arr?
+    let guess;
+    while (min <= max) {
+        // Find the mid index and round using Math.floor
+        let guess = Math.floor((min + max) / 2);
+
+        //If element is present at mid, return guess;
+        if (arr[guess] === targetValue) {
+            return `Number is ${targetValue} with index of ${guess}`;
+        } else if (arr[guess] < targetValue) {
+            min = guess + 1;
+        } else {
+            max = guess - 1;
+        }
+    }
+
+  return 'Number not found, -1';
+  // return index of location where the target value was found
 }
+
+const arrs = [4, 7, 10, 14, 18, 22, 34, 36, 44, 45, 50];
+console.log(binarySearch(arrs, 34));
+
