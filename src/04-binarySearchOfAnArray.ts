fb1343c6a48430c91739591a5f68415046845949
IngridGdesigns <garciai@protonmail.com>
2023-08-15 11:20:43 -0700

    added notes, created function

--- src/04-binarySearchOfAnArray.ts
+++ src/04-binarySearchOfAnArray.ts
@@ -2,10 +2,16 @@
 To-do
 pseudocode of binary search:
 
-Let min = 1 and max = n.
-Guess the average of max and min, rounded down so that it is an integer.
-If you guessed the number, stop. You found it!
-If the guess was too low, set min to be one larger than the guess.
-If the guess was too high, set max to be one smaller than the guess.
-Go back to step two.
+1 - Let min = 1 and max = n.
+2 - Guess the average of max and min, rounded down so that it is an integer.
+3 - If array[guess] equals target, stop. You found it! Return guess
+4 - If the guess was too low, array[guess] < target, 
+    set min to be one larger than the guess, (set min = guess + 1).
+5 - If the guess was too high, set max to be one smaller than the guess.
+    (set max = guess - 1)
+6 - Go back to step two.
 */
+
+function binarySearch(targetValue: number, array: string[]): number {
+    return 1;
+}
