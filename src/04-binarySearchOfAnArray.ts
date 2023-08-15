76d53b0ff23e9d960aabcf1e93815309820da588
IngridGdesigns <garciai@protonmail.com>
2023-08-15 11:05:39 -0700

    added file and pseudocode to-do

---
+++ src/04-binarySearchOfAnArray.ts
@@ -0,0 +1,11 @@
+/*
+To-do
+pseudocode of binary search:
+
+Let min = 1 and max = n.
+Guess the average of max and min, rounded down so that it is an integer.
+If you guessed the number, stop. You found it!
+If the guess was too low, set min to be one larger than the guess.
+If the guess was too high, set max to be one smaller than the guess.
+Go back to step two.
+*/
