8306fbfd9021b30c2e7fe1fc112522d3ba55d312
IngridGdesigns <garciai@protonmail.com>
2023-08-15 16:18:15 -0700

    updated notes

--- src/04-binarySearchOfAnArray.ts
+++ src/04-binarySearchOfAnArray.ts
@@ -4,7 +4,7 @@ pseudocode of binary search:
 
 1 - Let min = 1 and max = n.
 2 - If max<min, then stop; target is not present in array. Return -1.
-3 - Guess the average of max and min, rounded down so that it is an integer.
+3 - Guess the average of max and min, rounded down using Math.floor -  so that it is an integer.
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
 4 - If arr[guess] equals target, stop. You found it! Return guess
 5 - If the guess was too low, array[guess] < target, 
@@ -17,26 +17,22 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 function binarySearch(arr: number[], targetValue: number): string {
   let min = 0;
   let max = arr.length - 1;
-  //  What would happen if the number you are looking for is not in the arr?
-    let guess;
-    while (min <= max) {
-        // Find the mid index and round using Math.floor
-        let guess = Math.floor((min + max) / 2);
 
-        //If element is present at mid, return guess;
-        if (arr[guess] === targetValue) {
-            return `Number is ${targetValue} with index of ${guess}`;
-        } else if (arr[guess] < targetValue) {
-            min = guess + 1;
-        } else {
-            max = guess - 1;
-        }
+  let guess; // or mid
+  while (min <= max) {
+    let guess = Math.floor((min + max) / 2);
+
+    if (arr[guess] === targetValue) {
+      return `Number is ${targetValue} with index of ${guess}`;
+    } else if (arr[guess] < targetValue) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
     }
+  }
 
   return 'Number not found, -1';
-  // return index of location where the target value was found
 }
 
 const arrs = [4, 7, 10, 14, 18, 22, 34, 36, 44, 45, 50];
 console.log(binarySearch(arrs, 34));
-
