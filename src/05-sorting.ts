fc3dbf449fb370e683cb9c2fe5ca8f17fbd59df3
IngridGdesigns <garciai@protonmail.com>
2023-08-31 11:33:39 -0700

    added sorting problem

---
+++ src/05-sorting.ts
@@ -0,0 +1,19 @@
+/* Sorting!
+swapping locations of two items in an array 
+
+1 - loop 
+if firstindex is > secondindex, swap
+
+*/
+
+
+function swapping(numberArray: number[], firstIndex: number, secondIndex: number): number[] {
+    
+    return []
+}
+
+const arr1 = [9, 4, 7, 1, 2, 6, 5];
+
+let firstIndex = 2;
+let secondIndex = 0;
+swapping(arr1, firstIndex, secondIndex);
