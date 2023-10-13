25f80fbcec0a8b92dd90ca121fd9bfdcc6375228
IngridGdesigns <garciai@protonmail.com>
2023-10-13 02:33:42 -0700

    brute first solution

---
+++ src/07-charCounts.ts
@@ -0,0 +1,23 @@
+/* Write a function which takes in a string and returns counts of each character in the string
+- Expected output : "aloha" ---> {a:2, l:0, h:1}
+- Expected output : "bombero" ---> {b:2, o:2, m:1, e:1, r:1}
+
+
+steps:
+- loop through each character in string and count
+- if char is not in object add it and set value to 1,
+- if char is in object, add one to the object
+*/
+
+export function charCounts(str: string): object {
+  const letterCount: { [key: string]: any } = {};
+  for (let i = 0; i < str.length; i++) {
+    if (letterCount.hasOwnProperty(str[i])) {
+      // Wanted to use hasOwn()
+      letterCount[str[i]] += 1;
+    } else {
+      letterCount[str[i]] = 1;
+    }
+  }
+  return letterCount; // return object with keys that are lowercase characters
+}
