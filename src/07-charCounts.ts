7260e65c6e4a692956cdfde1047217d336bf269e
IngridGdesigns <garciai@protonmail.com>
2023-10-13 02:35:41 -0700

    refactored with ternary

--- src/07-charCounts.ts
+++ src/07-charCounts.ts
@@ -9,15 +9,27 @@ steps:
 - if char is in object, add one to the object
 */
 
+// export function charCounts(str: string): object {
+//   const letterCount: { [key: string]: any } = {};
+//   for (let i = 0; i < str.length; i++) {
+    // if (letterCount.hasOwnProperty(str[i])) {
+    //   // Wanted to use hasOwn()
+    //   letterCount[str[i]] += 1;
+    // } else {
+    //   letterCount[str[i]] = 1;
+    // }
+//   }
+//   return letterCount; // return object with keys that are lowercase characters
+// }
+
 export function charCounts(str: string): object {
   const letterCount: { [key: string]: any } = {};
   for (let i = 0; i < str.length; i++) {
-    if (letterCount.hasOwnProperty(str[i])) {
-      // Wanted to use hasOwn()
-      letterCount[str[i]] += 1;
-    } else {
-      letterCount[str[i]] = 1;
-    }
+    // refactored with ternary
+    const inputStr = str[i];
+    letterCount.hasOwnProperty(inputStr)
+      ? (letterCount[inputStr] += 1)
+      : (letterCount[inputStr] = 1);
   }
   return letterCount; // return object with keys that are lowercase characters
 }
