5b6fa3e218857e5c08c18cde0ddcec2158a77f9e
IngridGdesigns <garciai@protonmail.com>
2023-10-13 17:06:12 -0700

    added for of loop example

--- src/07-charCounts.ts
+++ src/07-charCounts.ts
@@ -12,16 +12,17 @@ steps:
 // export function charCounts(str: string): object {
 //   const letterCount: { [key: string]: any } = {};
 //   for (let i = 0; i < str.length; i++) {
-    // if (letterCount.hasOwnProperty(str[i])) { // if(letterCount[str[i] > 0])
-    //   // Wanted to use hasOwn()
-    //   letterCount[str[i]] += 1;
-    // } else {
-    //   letterCount[str[i]] = 1;
-    // }
+// if (letterCount.hasOwnProperty(str[i])) { // if(letterCount[str[i] > 0])
+//   // Wanted to use hasOwn()
+//   letterCount[str[i]] += 1;
+// } else {
+//   letterCount[str[i]] = 1;
+// }
 //   }
 //   return letterCount; // return object with keys that are lowercase characters
 // }
 
+// run npm test, test completed for this one
 export function charCounts(str: string): object {
   const letterCount: { [key: string]: any } = {};
 
@@ -39,4 +40,18 @@ export function charCounts(str: string): object {
   return letterCount; // return object with keys that are lowercase characters
 }
 
-// run npm test, test completed forthis one
+function charCountsUsingForOfLoop(str: string): object {
+  const letterCount: { [key: string]: any } = {};
+
+  for (let character of str) {
+    const regex = /[a-z0-9]/; // performance can vary with regex
+    const inputStr = character.toLowerCase();
+    if (regex.test(inputStr)) {
+      letterCount[inputStr] = ++letterCount[inputStr] || 1;
+    }
+  }
+  return letterCount;
+}
+
+console.log(charCountsUsingForOfLoop('Muito bom!'));
+// Expected output: { m: 2, u: 1, i: 1, t: 1, o: 2, b: 1 }
