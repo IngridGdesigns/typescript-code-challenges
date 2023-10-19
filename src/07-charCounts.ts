aea7f20c13c3633c67257c2d855cd207e10a85f4
IngridGdesigns <garciai@protonmail.com>
2023-10-19 11:48:13 -0700

    added practice function

--- src/07-charCounts.ts
+++ src/07-charCounts.ts
@@ -7,20 +7,44 @@ steps:
 - loop through each character in string and count
 - if char is not in object add it and set value to 1,
 - if char is in object, add one to the object
+
+
+
+// 1. Brute force solution
+export function charCountss(str: string): object {
+  const letterCount: { [key: string]: any } = {};
+  for (let i = 0; i < str.length; i++) {
+    if (letterCount.hasOwnProperty(str[i])) {
+      // if(letterCount[str[i] > 0])
+      // Wanted to use hasOwn()
+      letterCount[str[i]] += 1;
+    } else {
+      letterCount[str[i]] = 1;
+    }
+  }
+  return letterCount; // return object with keys that are lowercase characters
+}
+
+console.log(charCountss('yummy'));
 */
- //1. Brute force solution
-// export function charCounts(str: string): object {
-//   const letterCount: { [key: string]: any } = {};
-//   for (let i = 0; i < str.length; i++) {
-// if (letterCount.hasOwnProperty(str[i])) { // if(letterCount[str[i] > 0])
-//   // Wanted to use hasOwn()
-//   letterCount[str[i]] += 1;
-// } else {
-//   letterCount[str[i]] = 1;
-// }
-//   }
-//   return letterCount; // return object with keys that are lowercase characters
-// }
+
+function countLetters(letters: string): object {
+  const counted: { [key: string]: number } = {};
+
+  for (let char of letters) {
+    const lowercased = char.toLowerCase();
+    const regex = /[a-z0-9]/;
+    if (regex.test(lowercased)) {
+      // counted.hasOwnProperty(lowercased)
+      //   ? (counted[lowercased] += 1)
+      //   : (counted[lowercased] = 1);
+      counted[lowercased] = ++counted[lowercased] || +1;
+    }
+  }
+  return counted;
+}
+
+console.log(countLetters('mamaaaa'));
 
 // run npm test, test completed for this one
 export function charCounts(str: string): object {
