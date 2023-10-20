5fe3c4918f2c067bdcf0d168c8d65492a9e52fcc
IngridGdesigns <garciai@protonmail.com>
2023-10-20 12:47:09 -0700

    added test

--- src/__tests__/08-frequencyCounter.test.ts
+++ src/__tests__/08-frequencyCounter.test.ts
@@ -1,12 +1,15 @@
-import { same } from '../08-frequencyCounter';
+import { isFirstArrySameAsSecondArry } from '../08-frequencyCounter';
 
 describe('frequency counter function', () => {
   it('test', () => {
-    // const frequencyCounter1 = same([1, 2, 3], [4, 1, 9]);
-    const frequencyCounter2 = same([1, 2, 3], [1, 9]);
-    const frequencyCounter3 = same([1, 2, 1], [4, 4, 1]); // (must be same frequency)
+    const frequencyCounter1 = isFirstArrySameAsSecondArry(
+      [1, 2, 3, 2, 5],
+      [9, 1, 4, 4, 25]
+    );
+    const frequencyCounter2 = isFirstArrySameAsSecondArry([1, 2, 3], [1, 9]);
+    const frequencyCounter3 = isFirstArrySameAsSecondArry([1, 2, 1], [4, 4, 1]); // (must be same frequency)
 
-    // expect(frequencyCounter1).toBe(true);
+    expect(frequencyCounter1).toBe(true);
     expect(frequencyCounter2).toBe(false);
     expect(frequencyCounter3).toBe(false);
   });
