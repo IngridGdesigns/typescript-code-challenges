c1b2e11799df8055d28a8ff342a13d8544fec60d
IngridGdesigns <garciai@protonmail.com>
2023-10-23 12:32:54 -0700

    added note for refactoring in future

--- src/10-removingValuesFromArray.ts
+++ src/10-removingValuesFromArray.ts
@@ -3,7 +3,7 @@
 //brute force solution
 
 export function remove(arr: number[], item: number): Array<number> {
-  const newArr = [...arr];
+  const newArr = [...arr]; // maybe could use filter() in next iteration
   for (let i = 0; i < newArr.length; i++) {
     if (newArr[i] === item) {
       newArr.splice(i, 1);
