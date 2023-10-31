3761e0036b7a41ca3c3f08c8534bf9d772e1d1be
IngridGdesigns <garciai@protonmail.com>
2023-10-31 11:11:08 -0700

    fixed format

--- src/13-capatilizeFirstLetter.ts
+++ src/13-capatilizeFirstLetter.ts
@@ -4,11 +4,11 @@ Given an array of strings, return an array where
 the first letter of each string is capitalized
 */
 
-const names = ["florencia", "amado", "camilo", "dolores"]
+const names = ['florencia', 'amado', 'camilo', 'dolores'];
 // -->        ["Florencia", "Amado", "Camilo", "Dolores"]
 // Your code here
 const capitalized = names.map((name) => {
-    return name[0].toUpperCase() + name.slice(1)
-})
+  return name[0].toUpperCase() + name.slice(1);
+});
 
-console.log(capitalized)
+console.log(capitalized);
