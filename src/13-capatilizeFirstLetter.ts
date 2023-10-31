8cef84888d7e2f0e9235199022476d30c0ce6a53
IngridGdesigns <garciai@protonmail.com>
2023-10-31 11:10:52 -0700

    added new challenge

---
+++ src/13-capatilizeFirstLetter.ts
@@ -0,0 +1,14 @@
+/* 
+Challenge:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["florencia", "amado", "camilo", "dolores"]
+// -->        ["Florencia", "Amado", "Camilo", "Dolores"]
+// Your code here
+const capitalized = names.map((name) => {
+    return name[0].toUpperCase() + name.slice(1)
+})
+
+console.log(capitalized)
